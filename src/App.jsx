3c1267e8516804785335b44567c6102b7d20f8c5
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-10-04 21:04:14 +0100

    fixed react-router links

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import {createBrowserRouter, RouterProvider, Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Home';
 import About from './About';
 
 import './App.css'
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-]);
+
 
 function App() {
   
 
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
