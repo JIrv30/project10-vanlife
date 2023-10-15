2487cee3f1fb6c1872105b48510c41674b438a8c
JayIrv <135241031+JIrv30@users.noreply.github.com>
2023-10-15 17:07:22 +0100

    filled host vansdetail

--- src/pages/HostVanDetail.jsx
+++ src/pages/HostVanDetail.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 export default function HostVanDetail () {
 
+  const [hostVans, setHostVans] = useState(null)
+
+  const {id} = useParams()
+
+  useEffect(()=>{
+    fetch(`/api/host/vans/${id}`)
+    .then(res => res.json())
+    .then(data=>setHostVans(data.vans))
+  },[])
+
+  if(!hostVans) {
+    return <h1>Loading...</h1>
+  }
+
   return (
-    <h1>host van detail goes here</h1>
+    <section>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={hostVans.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${hostVans.type}`}>
+              {hostVans.type}
+            </i>
+            <h2>{hostVans.name}</h2>
+            <p>${hostVans.price}/day</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
